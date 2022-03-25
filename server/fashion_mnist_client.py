23aef60c49333ab8d01af958cbdb550fbb6e8e4c
pherna06 <pablohernandezaguado@gmail.com>
2022-03-25 20:12:13 +0100

    Added query functionality to a specific serving to query client.

--- server/fashion_mnist_client.py
+++ server/fashion_mnist_client.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import numpy as np
+import time
 
 _server_url = 'http://esfinge.dacya.ucm.es:5000'
 
@@ -8,15 +10,38 @@ def get_servings():
     response = requests.get(servings_uri)
     print(json.dumps(response.json(), indent=4))
 
+def query_serving():
+    size = int(input('> Input test query size: '))
+    serving_uri = input('> Input serving query URI: ')
+    print_response = input('> Print response? (y/n): ') == 'y'
+
+    try:
+        instances = np.random.rand(size, 28, 28, 1)
+        request_json = {
+            'signature_name' : 'serving_default' ,
+            'instances'      : np.random.rand(size, 28, 28, 1)
+        }
+
+        start = time.time()
+        response = requests.post(
+            serving_uri,
+            json = request_json )
+        elapsed_time = (time.time() - start) * 1000
+
+    print(f'Query elapsed time: {elapsed_time:.3f} ms')
+    if (print_response):
+        print(json.dumps(response.json(), indent=4))        
+
 _menu_actions = [
-    lambda : 0,
-    get_servings
-]
+    lambda : 0    ,
+    get_servings  ,
+    query_serving ]
 
 _menu_prompt = 'Client to perform "fashion_mnist"-like queries\n' \
               '----------------------------------------------\n' \
               'OPTIONS MENU:\n'                                  \
               '    1. Explore available servings.\n'             \
+              '    2. Make test query to a serving.\n'           \
               '\n'                                               \
               '    0. QUIT\n'                                    \
               '\n'                                               \
