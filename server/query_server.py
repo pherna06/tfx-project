c4ef4fd11fa5af3bf9b24aeb1a5eedff2ff3d700
pherna06 <pablohernandezaguado@gmail.com>
2022-03-25 19:10:10 +0100

    Fix typo.

--- server/query_server.py
+++ server/query_server.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 def import_servings_from_deployment_config(config_path):
-    with open('config_path', 'r') as deploy_yaml:
+    with open(config_path, 'r') as deploy_yaml:
         deployment = yaml.load(deploy_yaml, Loader=yaml.FullLoader)
 
     servings = {}
