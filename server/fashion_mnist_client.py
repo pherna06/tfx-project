ae49f96a0b41155dacbc71f1ce3fb9b4c35ee2ee
pherna06 <pablohernandezaguado@gmail.com>
2022-03-25 19:06:08 +0100

    Implemented initial server logic to discover servings.
    Moved servings deployment logic to 'deploy' folder.

---
+++ server/fashion_mnist_client.py
@@ -0,0 +1,36 @@
+import requests
+
+_server_url = 'https://esfinge.dacya.ucm.es:5000'
+
+def get_servings():
+    servings_uri = _server_url + '/servings'
+    response = requests.get(servings_url)
+    print(response.json())
+
+_menu_actions = [
+    lambda : pass,
+    get_servings
+]
+
+_menu_promt = 'Client to perform "fashion_mnist"-like queries\n' \
+              '----------------------------------------------\n' \
+              'OPTIONS MENU:\n'                                  \
+              '    1. Explore available servings.\n'             \
+              '\n'                                               \
+              '    0. QUIT\n'                                    \
+              '\n'                                               \
+              '> Input option :  '
+
+def client_menu():
+    opt = 0
+    while opt in _menu_actions
+        opt = input(_menu_prompt)
+        if opt == 0:
+            break
+        _menu_actions[opt]()
+
+def main():
+    client_menu()
+
+if __name__ == '__main__':
+    main()
---
+++ server/query_server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+def import_servings_from_deployment_config(config_path):
+    with open('config_path', 'r') as deploy_yaml:
+        deployment = yaml.load(deploy_yaml, Loader=yaml.FullLoader)
+
+    servings = {}
+    for i, deploy in enumerate(deployment):
+        servings[i] = {}
+        servings[i]['id'] = i
+        servings[i]['cont_name'] = deplot['model']['cont_name']
+        servings[i]['query_uri'] = deploy['model']['query_uri']
+
+    return servings
+
+_servings = import_servings_from_deployment_config('../deploy/deploymeny.yaml')
+
+@app.get('/servings')
+def get_servings():
+    return jsonify(servings)
+
